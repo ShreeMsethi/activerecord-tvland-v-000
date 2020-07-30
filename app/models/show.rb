5d0bfb5c794312fa1637e3aa47093a9477710848
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-30 22:26:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,13 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def actors_list
     self.actors
   end
   
-  def build_network
+  # def build_network
   
-  end
+  # end
 end
