5a418d2fed353032693cc38c4075eabac1b2468a
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-30 22:14:49 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,11 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
   
   def say_that_thing_you_say
-    "self#{always says:}"
-  end
-  
-  def catchphrase=( t = " ")
-    self.say_that_thing_you_say.t
+    "#{self.name} always says: #{self.catchphrase}"
   end
 
 end
